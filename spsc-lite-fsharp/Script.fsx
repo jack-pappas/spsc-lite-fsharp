2d170b06880afafe04f11c80387492cfaa7b1e56
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-10-11 23:09:52 -0400

    - Set up NuGet package restore on the solution. - Use FParsec and FsUnit packages from NuGet.

--- spsc-lite-fsharp/Script.fsx
+++
@@ -1,5 +0,0 @@
-﻿#r @"EnvDTE.dll"
-#r @"EnvDTE80.dll"
-open System.Runtime.InteropServices
-let appObj =Marshal.GetActiveObject("VisualStudio.DTE") :?> EnvDTE80.DTE2
-printfn "%s" (appObj.ActiveDocument.FullName)
